455616aa0399d9ac8a5ea8fbf0afc06419203212
Danil Perfilev <danilperfilev115@gmail.com>
2026-04-04 21:08:29 +0500

    Второй коммит: добавление функции printf

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ CC=clang  # Для Ubuntu: используйте CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
 # Сборка ядра
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \kernel.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \kernel.c common.c
 
 # Запуск QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \-kernel kernel.elf
